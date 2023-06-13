f7878b45039f566a2befc6bcfac9ff401776ebd4
Eltryo <david.merkl.dm@gmail.com>
2023-06-13 16:35:34 +0200

    Edit telescope nad vimtex config

--- after/plugin/vimtex.lua
+++ after/plugin/vimtex.lua
@@ -1,4 +1,3 @@
 vim.g.vimtex_compiler_method = 'latexmk'
---vim.g.vimtex_compiler_build_dir = 'build'
 vim.g.vimtex_compiler_latexmk = { build_dir = 'build'}
 vim.g.vimtex_compiler_latexmk_engines = { _ = '-lualatex'}
